cc682191761cbd3bcc1158dd17c37648cc7f670f
antluqmol1 <antonioton43@gmail.com>
2024-12-03 17:43:19 +0100

    Iniciando

--- src/Main.hs
+++
@@ -1,9 +0,0 @@
-main :: IO ()
-main = do
-    let agente1 = Agente "Agente 1" TitForTat 0
-    let agente2 = Agente "Agente 2" SiempreTraiciona 0
-    let rondas = 10
-    let (final1, final2) = simularJuego rondas agente1 agente2
-    putStrLn $ "Resultado final:"
-    putStrLn $ nombre final1 ++ ": " ++ show (puntaje final1) ++ " puntos"
-    putStrLn $ nombre final2 ++ ": " ++ show (puntaje final2) ++ " puntos"
